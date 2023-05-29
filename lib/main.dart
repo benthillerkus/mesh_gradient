e85625c2b67ada23baeda4b0b8a84418fca0e8e0
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2023-05-29 14:21:47 +0200

    step: draw a triangle

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'dart:ui';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:mess_gradients/conversions.dart';
+import 'package:mess_gradients/dot.dart';
 import 'package:mess_gradients/picker.dart';
 import 'package:flutter_color_models/flutter_color_models.dart';
+import 'package:collection/collection.dart';
 
 late FragmentShader fragmentShader;
 late FragmentShader pickerFragmentShader;
@@ -32,9 +34,12 @@ class Home extends HookWidget {
         useState(OklabColor.fromColor(const Color.fromARGB(255, 56, 192, 108)));
     final color2 =
         useState(OklabColor.fromColor(const Color.fromARGB(255, 123, 02, 189)));
+    final color3 =
+        useState(OklabColor.fromColor(const Color.fromARGB(255, 216, 185, 47)));
 
     final pickerPosition = useState<Offset?>(null);
     final picker2Position = useState<Offset?>(null);
+    final picker3Position = useState<Offset?>(null);
 
     return Center(
       child: SizedBox.square(
@@ -44,8 +49,20 @@ class Home extends HookWidget {
               Positioned.fill(
                 child: CustomPaint(
                   painter: _Painter(
-                    color.value.lch,
-                    color2.value.lch,
+                    offset: Offset(
+                      const DotThemeData().radius,
+                      const DotThemeData().radius,
+                    ),
+                    [
+                      pickerPosition.value ?? Offset.zero,
+                      picker2Position.value ?? Offset.zero,
+                      picker3Position.value ?? Offset.zero,
+                    ],
+                    [
+                      color.value,
+                      color2.value,
+                      color3.value,
+                    ],
                   ),
                 ),
               ),
@@ -54,7 +71,8 @@ class Home extends HookWidget {
                 top: pickerPosition.value?.dy,
                 child: Listener(
                   onPointerMove: (details) {
-                    pickerPosition.value = (pickerPosition.value ?? Offset.zero) + details.delta;
+                    pickerPosition.value =
+                        (pickerPosition.value ?? Offset.zero) + details.delta;
                   },
                   child: PickerDot(
                       color: color.value,
@@ -66,11 +84,25 @@ class Home extends HookWidget {
                 top: picker2Position.value?.dy,
                 child: Listener(
                   onPointerMove: (details) {
-                    picker2Position.value = (picker2Position.value ?? Offset.zero) + details.delta;
+                    picker2Position.value =
+                        (picker2Position.value ?? Offset.zero) + details.delta;
                   },
                   child: PickerDot(
                       color: color2.value,
                       onColorChanged: (cl) => color2.value = cl),
+                ),
+              ),
+              Positioned(
+                left: picker3Position.value?.dx,
+                top: picker3Position.value?.dy,
+                child: Listener(
+                  onPointerMove: (details) {
+                    picker3Position.value =
+                        (picker3Position.value ?? Offset.zero) + details.delta;
+                  },
+                  child: PickerDot(
+                      color: color3.value,
+                      onColorChanged: (cl) => color3.value = cl),
                 ),
               )
             ],
@@ -80,25 +112,24 @@ class Home extends HookWidget {
 }
 
 class _Painter extends CustomPainter {
-  const _Painter(this.color, this.color2);
-  final LCH color;
-  final LCH color2;
+  const _Painter(this.positions, this.colors, {this.offset = Offset.zero});
+  final List<OklabColor> colors;
+  final List<Offset> positions;
+  final Offset offset;
 
   @override
   void paint(Canvas canvas, Size size) {
-    fragmentShader.setFloat(0, size.width);
-    fragmentShader.setFloat(1, size.height);
-    fragmentShader.setFloat(2, color.lightness);
-    fragmentShader.setFloat(3, color.chroma);
-    fragmentShader.setFloat(4, color.hue);
-    fragmentShader.setFloat(5, color2.lightness);
-    fragmentShader.setFloat(6, color2.chroma);
-    fragmentShader.setFloat(7, color2.hue);
-    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height),
-        Paint()..shader = fragmentShader);
+    final vertices = Vertices(
+      VertexMode.triangles,
+      positions.map((e) => e + offset).toList(),
+      colors: colors.map((e) => e.toColor()).toList(),
+    );
+    canvas.drawVertices(vertices, BlendMode.dstOver, Paint());
   }
 
   @override
   bool shouldRepaint(_Painter oldDelegate) =>
-      color != oldDelegate.color || color2 != oldDelegate.color2;
+      offset != oldDelegate.offset ||
+      !colors.equals(oldDelegate.colors) ||
+      !positions.equals(oldDelegate.positions);
 }
