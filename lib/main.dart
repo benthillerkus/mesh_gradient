9d4b163bbbcb243b2bd07e2e03a48d7ca940b003
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2023-05-29 21:06:07 +0200

    step: stuck on catmull-clark

--- lib/main.dart
+++ lib/main.dart
@@ -32,16 +32,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final colors = <OklabColor>[
-    OklabColor.fromColor(const Color.fromARGB(255, 123, 02, 189)),
-    OklabColor.fromColor(const Color.fromARGB(255, 62, 206, 5)),
-    OklabColor.fromColor(const Color.fromARGB(255, 221, 224, 22)),
-  ];
-
   final positions = <Alignment>[
     Alignment.topLeft,
+    Alignment.topCenter,
     Alignment.topRight,
-    Alignment.bottomCenter
+    Alignment.centerLeft,
+    Alignment.center,
+    Alignment.centerRight,
+    Alignment.bottomLeft,
+    Alignment.bottomCenter,
+    Alignment.bottomRight,
+  ];
+
+  final colors = <OklabColor>[
+    OklabColor.fromColor(const Color.fromARGB(255, 255, 122, 122)),
+    OklabColor.fromColor(const Color.fromARGB(255, 255, 122, 122)),
+    OklabColor.fromColor(const Color.fromARGB(255, 255, 122, 122)),
+    OklabColor.fromColor(const Color.fromARGB(255, 255, 122, 122)),
+    OklabColor.fromColor(const Color.fromARGB(255, 255, 122, 122)),
+    OklabColor.fromColor(const Color.fromARGB(255, 255, 122, 122)),
+    OklabColor.fromColor(const Color.fromARGB(255, 255, 122, 122)),
+    OklabColor.fromColor(const Color.fromARGB(255, 255, 122, 122)),
+    OklabColor.fromColor(const Color.fromARGB(255, 255, 122, 122)),
   ];
 
   final centerDotOffset = Offset(
@@ -62,6 +74,8 @@ class _HomeState extends State<Home> {
                     painter: _Painter(
                       positions,
                       colors,
+                      rows: 3,
+                      columns: 3,
                     ),
                   ),
                 ),
@@ -102,18 +116,172 @@ extension on Offset {
       Alignment(dx / size.width * 2 - 1, dy / size.height * 2 - 1);
 }
 
+typedef Quad = ({int topLeft, int topRight, int bottomLeft, int bottomRight});
+typedef QuadGrid = List<List<Quad>>;
+typedef Edge = ({int start, int end});
+
+QuadGrid quadsFromControlPoints(int rows, int columns) {
+  final grid = <List<Quad>>[];
+  for (int i = 0; i < rows - 1; i++) {
+    final row = <Quad>[];
+    for (int j = 0; j < columns - 1; j++) {
+      final topLeft = i * columns + j;
+      final topRight = topLeft + 1;
+      final bottomLeft = topLeft + columns;
+      final bottomRight = bottomLeft + 1;
+      row.add((
+        topLeft: topLeft,
+        topRight: topRight,
+        bottomRight: bottomRight,
+        bottomLeft: bottomLeft
+      ));
+    }
+    grid.add(row);
+  }
+  return grid;
+}
+
+// List<Edge> edgesFromFaces(List<Face> faces) {
+//   final Set<Edge> edges = {};
+//   for (final face in faces) {
+//     for (int i = 0; i < face.length; i++) {
+//       edges.add((
+//         start: face[i],
+//         end: face[(i + 1) % face.length],
+//       ));
+//     }
+//   }
+//   return edges.toList();
+// }
+
+Iterable<List<int>> triangulateQuads(Iterable<Quad> quadFaces) sync* {
+  for (final quadFace in quadFaces) {
+    yield [quadFace.topLeft, quadFace.topRight, quadFace.bottomRight];
+    yield [quadFace.topLeft, quadFace.bottomRight, quadFace.bottomLeft];
+  }
+}
+
+(List<Offset>, List<ColorModel>, List<Quad>) subdivideCatmullClark(
+    List<Offset> positions, List<ColorModel> colors, QuadGrid faces) {
+  final facePoints = [
+    for (final face in faces.flattened)
+      (positions[face.topLeft] +
+              positions[face.topRight] +
+              positions[face.bottomLeft] +
+              positions[face.bottomRight]) *
+          0.25
+  ];
+
+  final edgePoints = <Offset>[];
+  final positionEdgeMidpoints = List<Set<Offset>?>.filled(positions.length, null, growable: false);
+  final rows = faces.length;
+  final columns = faces[0].length;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
+      final face = faces[i][j];
+      final facePoint = facePoints[i * columns + j];
+
+      final topMidpoint = positions[face.topLeft] +
+          (positions[face.topRight] - positions[face.topLeft]) * 0.5;
+      final rightMidpoint = positions[face.topRight] +
+          (positions[face.bottomRight] - positions[face.topRight]) * 0.5;
+      final bottomMidpoint = positions[face.bottomLeft] +
+          (positions[face.bottomRight] - positions[face.bottomLeft]) * 0.5;
+      final leftMidpoint = positions[face.topLeft] +
+          (positions[face.bottomLeft] - positions[face.topLeft]) * 0.5;
+
+      face.topLeftMidPoint = topMidpoint;
+
+      edgeMidPoints.add(topMidpoint);
+      edgeMidPoints.add(rightMidpoint);
+      edgeMidPoints.add(bottomMidpoint);
+      edgeMidPoints.add(leftMidpoint);
+
+      final top = facePoints.elementAtOrNull((i - 1) * columns + j);
+      final right = facePoints.elementAtOrNull(i * columns + j + 1);
+      final bottom = facePoints.elementAtOrNull((i + 1) * columns + j);
+      final left = facePoints.elementAtOrNull(i * columns + j - 1);
+
+      final topEdgePoint = top == null
+          ? topMidpoint
+          : (((top + facePoint) / 2) + topMidpoint) / 2;
+      final rightEdgePoint = right == null
+          ? rightMidpoint
+          : (((right + facePoint) / 2) + rightMidpoint) / 2;
+      final bottomEdgePoint = bottom == null
+          ? bottomMidpoint
+          : (((bottom + facePoint) / 2) + bottomMidpoint) / 2;
+      final leftEdgePoint = left == null
+          ? leftMidpoint
+          : (((left + facePoint) / 2) + leftMidpoint) / 2;
+
+      edgePoints.add(topEdgePoint);
+      edgePoints.add(rightEdgePoint);
+      edgePoints.add(bottomEdgePoint);
+      edgePoints.add(leftEdgePoint);
+    }
+  }
+
+  final newPositions = <Offset>[];
+  final newColors = <ColorModel>[];
+
+  for (int i = 0; i < positions.length; i++) {
+    final position = positions[i];
+    final color = colors[i];
+
+    // 1. average of face points for faces touching this vertex
+    final currentRow = i ~/ columns;
+    final topLeftFacePoint =
+        facePoints.elementAtOrNull(i - columns - currentRow - 1);
+    final topRightFacePoint =
+        facePoints.elementAtOrNull(i - columns - currentRow);
+    final bottomLeftFacePoint = facePoints.elementAtOrNull(i - currentRow - 1);
+    final bottomRightFacePoint = facePoints.elementAtOrNull(i - currentRow);
+    final allFacePoints = [
+      topLeftFacePoint,
+      topRightFacePoint,
+      bottomLeftFacePoint,
+      bottomRightFacePoint
+    ].whereType<Offset>().toList();
+    final averageFacePoint =
+        allFacePoints.reduce((a, b) => a + b) / facePoints.length.toDouble();
+
+    // 2. average of edge midpoints for edges touching this vertex
+    final rightEdgeMidPoint =
+        edgeMidPoints.elementAtOrNull((i - currentRow) * 4) ??
+            edgeMidPoints.elementAtOrNull((i - currentRow - 1) * 4 + 2);
+    final bottomEdgeMidPoint =
+        edgeMidPoints.elementAtOrNull((i - currentRow) * 4 + 3) ??
+            edgeMidPoints.elementAtOrNull((i - currentRow - 1) * 4 + 1);
+    final leftEdgeMidPoint =
+        edgeMidPoints.elementAtOrNull((i - currentRow - 1) * 4);
+    final topEdgeMidPoint =
+        edgeMidPoints.elementAtOrNull((i - currentRow - 1) * 4 + 1);
+  }
+}
+
 class _Painter extends CustomPainter {
-  const _Painter(this.positions, this.colors);
-  final List<OklabColor> colors;
+  const _Painter(this.positions, this.colors,
+      {required this.rows, required this.columns});
+  final List<ColorModel> colors;
   final List<Alignment> positions;
+  final int rows;
+  final int columns;
 
   @override
   void paint(Canvas canvas, Size size) {
-    final vertices = Vertices(
-      VertexMode.triangles,
-      positions.map((e) => e.alongSize(size)).toList(),
-      colors: colors.map((e) => e.toColor()).toList(),
-    );
+    final quads = quadsFromControlPoints(rows, columns);
+    final triangles = triangulateQuads(quads.flattened);
+    final positions = <Offset>[];
+    final colors = <Color>[];
+    for (final triangle in triangles) {
+      for (final index in triangle) {
+        positions.add(this.positions[index].alongSize(size));
+        colors.add(this.colors[index].toColor());
+      }
+    }
+
+    final vertices = Vertices(VertexMode.triangles, positions, colors: colors);
     canvas.drawVertices(vertices, BlendMode.dstOver, Paint());
   }
 
