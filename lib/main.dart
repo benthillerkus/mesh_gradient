15247c9967e278d21c13211f1976c6518100ebec
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2023-05-29 15:14:45 +0200

    step: use relative coordinates

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'dart:ui';
 
+import 'package:collection/collection.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_color_models/flutter_color_models.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:mess_gradients/conversions.dart';
 import 'package:mess_gradients/dot.dart';
 import 'package:mess_gradients/picker.dart';
-import 'package:flutter_color_models/flutter_color_models.dart';
-import 'package:collection/collection.dart';
 
 late FragmentShader fragmentShader;
 late FragmentShader pickerFragmentShader;
@@ -25,103 +24,94 @@ Future<void> main() async {
   ));
 }
 
-class Home extends HookWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final colors = <OklabColor>[
+    OklabColor.fromColor(const Color.fromARGB(255, 123, 02, 189)),
+    OklabColor.fromColor(const Color.fromARGB(255, 62, 206, 5)),
+    OklabColor.fromColor(const Color.fromARGB(255, 221, 224, 22)),
+  ];
+
+  final positions = <Alignment>[
+    Alignment.topLeft,
+    Alignment.topRight,
+    Alignment.bottomCenter
+  ];
+
+  final centerDotOffset = Offset(
+    -const DotThemeData().radius,
+    -const DotThemeData().radius,
+  );
+
+  @override
   Widget build(BuildContext context) {
-    final color =
-        useState(OklabColor.fromColor(const Color.fromARGB(255, 56, 192, 108)));
-    final color2 =
-        useState(OklabColor.fromColor(const Color.fromARGB(255, 123, 02, 189)));
-    final color3 =
-        useState(OklabColor.fromColor(const Color.fromARGB(255, 216, 185, 47)));
-
-    final pickerPosition = useState<Offset?>(null);
-    final picker2Position = useState<Offset?>(null);
-    final picker3Position = useState<Offset?>(null);
-
     return Center(
       child: SizedBox.square(
           dimension: 400,
-          child: Stack(
-            children: [
-              Positioned.fill(
-                child: CustomPaint(
-                  painter: _Painter(
-                    offset: Offset(
-                      const DotThemeData().radius,
-                      const DotThemeData().radius,
+          child: LayoutBuilder(builder: (context, constraints) {
+            return Stack(
+              children: [
+                Positioned.fill(
+                  child: CustomPaint(
+                    painter: _Painter(
+                      positions,
+                      colors,
                     ),
-                    [
-                      pickerPosition.value ?? Offset.zero,
-                      picker2Position.value ?? Offset.zero,
-                      picker3Position.value ?? Offset.zero,
-                    ],
-                    [
-                      color.value,
-                      color2.value,
-                      color3.value,
-                    ],
                   ),
                 ),
-              ),
-              Positioned(
-                left: pickerPosition.value?.dx,
-                top: pickerPosition.value?.dy,
-                child: Listener(
-                  onPointerMove: (details) {
-                    pickerPosition.value =
-                        (pickerPosition.value ?? Offset.zero) + details.delta;
-                  },
-                  child: PickerDot(
-                      color: color.value,
-                      onColorChanged: (cl) => color.value = cl),
-                ),
-              ),
-              Positioned(
-                left: picker2Position.value?.dx,
-                top: picker2Position.value?.dy,
-                child: Listener(
-                  onPointerMove: (details) {
-                    picker2Position.value =
-                        (picker2Position.value ?? Offset.zero) + details.delta;
-                  },
-                  child: PickerDot(
-                      color: color2.value,
-                      onColorChanged: (cl) => color2.value = cl),
-                ),
-              ),
-              Positioned(
-                left: picker3Position.value?.dx,
-                top: picker3Position.value?.dy,
-                child: Listener(
-                  onPointerMove: (details) {
-                    picker3Position.value =
-                        (picker3Position.value ?? Offset.zero) + details.delta;
-                  },
-                  child: PickerDot(
-                      color: color3.value,
-                      onColorChanged: (cl) => color3.value = cl),
-                ),
-              )
-            ],
-          )),
+                for (int i = 0; i < colors.length; i++)
+                  Positioned(
+                    left:
+                        (positions[i].x / 2 + 0.5) * constraints.biggest.width,
+                    top:
+                        (positions[i].y / 2 + 0.5) * constraints.biggest.height,
+                    child: Transform.translate(
+                      offset: centerDotOffset,
+                      child: Listener(
+                        onPointerMove: (details) {
+                          setState(() {
+                            final newPosition =
+                                (positions[i].alongSize(constraints.biggest) +
+                                    details.delta);
+                            positions[i] =
+                                newPosition.alignmentIn(constraints.biggest);
+                          });
+                        },
+                        child: PickerDot(
+                            color: colors[i],
+                            onColorChanged: (cl) =>
+                                setState(() => colors[i] = cl)),
+                      ),
+                    ),
+                  ),
+              ],
+            );
+          })),
     );
   }
 }
 
+extension on Offset {
+  Alignment alignmentIn(Size size) =>
+      Alignment(dx / size.width * 2 - 1, dy / size.height * 2 - 1);
+}
+
 class _Painter extends CustomPainter {
-  const _Painter(this.positions, this.colors, {this.offset = Offset.zero});
+  const _Painter(this.positions, this.colors);
   final List<OklabColor> colors;
-  final List<Offset> positions;
-  final Offset offset;
+  final List<Alignment> positions;
 
   @override
   void paint(Canvas canvas, Size size) {
     final vertices = Vertices(
       VertexMode.triangles,
-      positions.map((e) => e + offset).toList(),
+      positions.map((e) => e.alongSize(size)).toList(),
       colors: colors.map((e) => e.toColor()).toList(),
     );
     canvas.drawVertices(vertices, BlendMode.dstOver, Paint());
@@ -129,7 +119,6 @@ class _Painter extends CustomPainter {
 
   @override
   bool shouldRepaint(_Painter oldDelegate) =>
-      offset != oldDelegate.offset ||
       !colors.equals(oldDelegate.colors) ||
       !positions.equals(oldDelegate.positions);
 }
